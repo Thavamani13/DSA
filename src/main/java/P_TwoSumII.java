8b0b3fb0435390e2e1556020a4a5a7382f6149f2
Thavamani Murugan <thavamani1304@gmail.com>
2023-10-24 11:43:27 +0530

    Solved two sum 11 using two pointer approach

--- src/main/java/P_TwoSumII.java
+++ src/main/java/P_TwoSumII.java
@@ -11,10 +11,22 @@ public class P_TwoSumII {
         assertEquals(twoSum(nums,target),new int[]{1,2});
     }
 
-    /*
+    @Test
+    public  void testcase2(){
+        int[] nums = new int[]{2,7,11,15};
+        int target =9;
+        assertEquals(twoSumTwoPointers(nums,target),new int[]{1,2});
+    }
+
+
+
+    /*Since the inputs are sorted
+    *The outer for loop can be defined as constent
+    *    inner loop will traverse through the array and find the targte(left+rightval==target) return indeces
+    *    Since it is based index , return indeces+1
+    * defaul return {-1,-1}
     *
-    *
-    *
+    *Time - O(n2) , Space - O(1)
     * */
     private int[] twoSum(int[] nums, int target) {
 
@@ -24,6 +36,38 @@ public class P_TwoSumII {
             for (int j = i+1; j < nums.length; j++) {
                 int rightVal = nums[j];
                 if(leftVal+rightVal==target) return new int[]{i+1,j+1};
+            }
+        }
+
+        return new int[]{-1,-1};
+    }
+
+
+    /*Since the inputs are sorted
+    * keep two pointers , left and right extreeme
+    * Aadd both val => left + right == target return {leftindex+, right index+}
+    * Add both Val < target ==>increment left pointer
+    * Add both Val > target ==>decrement right pointer
+    * Continute till leftpointer==right pointer , return -1,-1 by default
+    * TC - O(n)
+    * SC - O(1)
+    * */
+    private int[] twoSumTwoPointers(int[] nums, int target) {
+        int left=0,right=nums.length-1;
+        while(left<right){
+            int leftVal = nums[left];
+            int rightVal = nums[right];
+
+            if(leftVal+rightVal<target){
+                left++;
+            }
+
+            else if(leftVal+rightVal>target){
+                right--;
+            }
+
+            else {
+                return new int[]{left+1,right+1};
             }
         }
 
